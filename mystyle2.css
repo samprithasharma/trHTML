25238ad222c72e99a2360270b264707061644869
samprithasharma <47626107+samprithasharma@users.noreply.github.com>
2019-12-12 20:39:27 -0800

    Add files via upload

---
+++ mystyle2.css
@@ -0,0 +1,22 @@
+#t
+{
+    text-shadow: 5px 5px 5px darkslategrey;
+    text-align: center;
+   
+    border-style: double;
+    height: 40px;
+    border: 2px solid;
+    border-width: 10px;
+    border-color: blue;
+    border-radius: 55px;
+    background-color: magenta;
+
+}
+#article
+{
+    width: 1000px;
+    height: 1000px;
+    background-color: darkcyan;
+    text-align: center;
+    
+}
